f029065bc50d1fd7064930dcef1ad8fef11d56de
laksh.j <lekshmanan.j7@gmail.com>
2025-02-25 17:06:27 +0530

    first commit

---
+++ src/NewGuviProject/AddNumber.java
@@ -0,0 +1,13 @@
+package NewGuviProject;
+
+public class AddNumber {
+
+	public static void main(String[] args) {
+		int number1 = 50;
+		int number2 = 20;
+		int subtraction = number1-number2;
+		System.out.println("the sum of number is::" + subtraction);
+
+	}
+
+}
---
+++ src/NewGuviProject/FirstJavaProject.java
@@ -0,0 +1,13 @@
+package NewGuviProject;
+
+public class FirstJavaProject {
+
+	public static void main(String[] args) {
+		System.out.println("HelloArun");
+		sum();
+
+	}
+	public static void sum() {
+		System.out.println("I am sum method");
+	}
+}
